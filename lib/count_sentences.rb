ff8e229f3d5501e32d4b0d311c3c33e60980288e
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-08 23:06:04 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1]=="."
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+ if self.include?(",")
+  self.split(",").count
+ elsif (self.include?(".")  || self.include?(" ")) && !self.include?(",")
+  self.split.count
+ else
+  self.split.count
+ end
   end
 end
